1863e1485aff87702d83284bedd76980e74499b5
Shiv Desai <shivdesai757@gmail.com>
2024-01-05 19:50:37 -0500

    added register and login page with html/css files

--- expense.py
+++ expense.py
@@ -1,9 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+import MySQLdb
+import re
 
-applicationName = Flask(__name__)
 
-@applicationName.route("/")
-def helloWorld(name=None):
-    return render_template("test.html", name=name)
+app = Flask(__name__)
+app.secret_key = 'ebx71384hjkdfewk'
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'Moraxwashere7@'
+app.config['MYSQL_DB'] = 'expdb'
+
+mysql = MySQL(app)
+
+
+@app.route("/budgetmate/login", methods=['GET','POST'])
+def login():
+    msg = ''
+    return render_template("loginpage.html")
+
+@app.route("/budgetmate/register", methods=['GET','POST'])
+def register():
+    msg = ''
+    return render_template("register.html")
+
+
 
 
